0cd055a41e4fc8b5425f9fe6deed36c65d949dad
zjttzxccx <zjttzxccx@163.com>
2020-03-12 10:26:54 +0800

    直接将标题增加链接，点击标题就可跳转到网页
    直接将标题增加链接，点击标题就可跳转到网页

--- Announcemail-sizheng-final2.py
+++ Announcemail-sizheng-final2.py
@@ -17,22 +17,23 @@ from selenium import webdriver
 #邮件发送模块
 def sent_email(mail_body):
     sender = '0010467@zju.edu.cn'
-    receiver = ['wxh123@zju.edu.cn','yanxiliu@zju.edu.cn','ccp@zju.edu.cn','jsj1126@zju.edu.cn']
-    #receiver=['ccx@zju.edu.cn','0010467@zju.edu.cn']
+    receiver = ['xujie@zju.edu.cn','jiahonglei@zju.edu.cn']
+    #receiver = ['ccx@zju.edu.cn','0010467@zju.edu.cn']
     smtpServer = 'smtp.zju.edu.cn'
     username = '0010467'
     password = '47530471shine'
     if datetime.date.today().isoweekday()==1:
-    	mail_title=(datetime.date.today()-datetime.timedelta(days=3)).__format__('%Y-%m-%d')+'至'+(datetime.date.today()-datetime.timedelta(days=1)).__format__('%Y-%m-%d')+'（周五至周日）最新通知'
+    	mail_title=(datetime.date.today()-datetime.timedelta(days=3)).__format__('%Y-%m-%d')+'至'+(datetime.date.today()-datetime.timedelta(days=1)).__format__('%Y-%m-%d')+'（周五至周日）最新通知（思政）'
     else:
     	mail_title = (datetime.date.today()-datetime.timedelta(days=1)).__format__('%Y-%m-%d')+'最新通知（思政）'
     mail_body = mail_body
 
-    message = MIMEText(mail_body, 'plain', 'utf-8')
+    message = MIMEText(mail_body, 'html', 'utf-8')
     message["Accept-Language"] = "zh-CN"
     message["Accept-Charset"] = "ISO-8859-1,utf-8"
     message['From'] = sender
     message['To'] =",".join(receiver)
+    #message['To'] =receiver
     message['Subject'] = Header(mail_title, 'utf-8')
 
     try:
@@ -79,13 +80,57 @@ def get_content_xgb():
 				for i in range(1,4):
 					current_time=(today_time-datetime.timedelta(days=i)).__format__('%Y-%m-%d')
 					if art_time==current_time:
-						content_list.append('学工部通知：'+content+' 链接地址:'+main_url+art_url+' 时间：'+art_time)
+						content_list.append('学工部通知：<a href='+str(main_url+art_url)+'>'+content+'</a> 时间：'+art_time)
 			else:
 				current_time=(today_time-datetime.timedelta(days=1)).__format__('%Y-%m-%d')
 		#current_time=(datetime.date.today()-datetime.timedelta(days=1)).__format__('%Y-%m-%d')
 				if art_time==current_time:
-					content_list.append('学工部通知：'+content+' 链接地址:'+main_url+art_url)
+					content_list.append('学工部通知：<a href='+str(main_url+art_url)+'>'+content+'</a>')
 		return content_list
+
+def get_content_ygb():
+	socket.setdefaulttimeout(20)  # 设置socket层的超时时间为20秒
+	url ='http://ygb.zju.edu.cn/redir.php?catalog_id=140080'
+	head={}
+	head['User-Agent']='Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+	try:
+		r = requests.get(url=url,headers=head)
+		r.encoding='gb2312'
+	except requests.exceptions.ConnectionError:
+		content_list = ['研工部连接错误']
+		print('研工部连接错误，请等待2秒')
+		time.sleep(2)	# 自定义
+		return content_list
+	except:
+		content_list = ['研工部连接错误']
+		print('研工部发生未知错误，请等待2秒')
+		time.sleep(2)	# 自定义
+		return content_list
+	else:
+		soup = BeautifulSoup(r.text,'lxml')
+		r.close()
+		time.sleep(0.1)	# 自定义
+		links=soup.find('ul',class_="common-news-list")
+		trlinks=links.find_all('li')
+		main_url='http://ygb.zju.edu.cn/'
+		#print(trlinks)
+		today_time=datetime.date.today()
+		content_list=[]
+		for trlink in trlinks:
+			art_url=trlink.a.get('href')
+			art_time=trlink.find('span',class_="date").text
+			content=trlink.a.get('title')
+			#current_time=(today_time-datetime.timedelta(days=1)).__format__('%Y-%m-%d')
+			if today_time.isoweekday()==1:
+				for i in range(1,4):
+					current_time=(today_time-datetime.timedelta(days=i)).__format__('%Y-%m-%d')
+					if art_time==current_time:
+						content_list.append('研工部通知：<a href='+str(main_url+art_url)+'>'+content+'</a> 时间：'+art_time)
+			else:
+				current_time=(today_time-datetime.timedelta(days=1)).__format__('%Y-%m-%d')
+				if art_time==current_time:
+					content_list.append('研工部通知：<a href='+str(main_url+art_url)+'>'+content+'</a>')
+		return content_list	
 
 def get_content_tw():
 	socket.setdefaulttimeout(20)  # 设置socket层的超时时间为20秒
@@ -124,12 +169,12 @@ def get_content_tw():
 				for i in range(1,4):
 					current_time='['+(today_time-datetime.timedelta(days=i)).__format__('%Y-%m-%d')+']'
 					if art_time==current_time:
-						content_list.append('校团委通知：'+content+' 链接地址:'+main_url+art_url+' 时间：'+art_time)
+						content_list.append('校团委通知：<a href='+str(main_url+art_url)+'>'+content+'</a> 时间：'+art_time)
 			else:
 				current_time='['+(datetime.date.today()-datetime.timedelta(days=1)).__format__('%Y-%m-%d')+']'
 				#current_time=(datetime.date.today()-datetime.timedelta(days=1)).__format__('%Y-%m-%d')
 				if art_time==current_time:
-					content_list.append('校团委通知：'+content+' 链接地址:'+main_url+art_url)
+					content_list.append('校团委通知：<a href='+str(main_url+art_url)+'>'+content+'</a>')
 		return content_list
 
 def get_content_jyzx():
@@ -174,46 +219,55 @@ def get_content_jyzx():
 				for i in range(1,4):
 					current_time=(today_time-datetime.timedelta(days=i)).__format__('%Y-%m-%d')
 					if art_time==current_time:
-						content_list.append('就业中心通知：'+content+' 链接地址:'+art_url+' 时间：'+art_time)
+						content_list.append('就业中心通知：<a href='+str(art_url)+'>'+content+'</a> 时间：'+art_time)
 			else:
 				current_time=(today_time-datetime.timedelta(days=1)).__format__('%Y-%m-%d')
 				#current_time=(datetime.date.today()-datetime.timedelta(days=1)).__format__('%Y-%m-%d')
 				if art_time==current_time:
-					content_list.append('就业中心通知：'+content+' 链接地址:'+art_url)
+					content_list.append('就业中心通知：<a href='+str(art_url)+'>'+content+'</a>')
 		return content_list
 
-if __name__ == '__main__':
-	'''url ='http://www.xgb.zju.edu.cn/default.html'
+def get_weather():
+	today_time=datetime.date.today()
+	socket.setdefaulttimeout(10)
+	url='http://t.weather.sojson.com/api/weather/city/101210101'
 	head={}
 	head['User-Agent']='Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
-	r = requests.get(url=url,headers=head)
-	r.encoding='gb2312'
-	soup = BeautifulSoup(r.text,'lxml')
-	links = soup.find('div',class_="con")
-	alinks=links.find_all('li')
-	content_list=[]
-	for alink in alinks:
-		art_url=alink.a.get('href')
-		content=alink.a.get('title')
-		art_time=alink.find('span',class_="time").text.strip('\n')
-		current_time=datetime.date.today().__format__('%Y-%m-%d')
-		if art_time==current_time:
-			content_list.append(content+'/'+art_time)
-	print(content_list)'''
+	data = requests.get(url=url,headers=head).text
+	jdata=json.loads(data)
+	wearher_data=jdata.get("data")
+	forecast=wearher_data.get("forecast")
+	high_temp=forecast[1].get("high")
+	low_temp=forecast[1].get("low")
+	sunrise=forecast[1].get("sunrise")
+	sunset=forecast[1].get("sunset")
+	fx=forecast[1].get("fx")
+	fl=forecast[1].get("fl")
+	wearher_type=forecast[1].get("type")
+	notice=forecast[1].get("notice")
+	print(notice)
+	weather_content=[]
+	weather_content.append('<h4>明日('+str(today_time+datetime.timedelta(days=1))+')杭州天气预报：</h4>'+'天气：'+wearher_type+'  风向：'+fx+'  风力：'+fl+'<br><font color=red><strong>'+high_temp+'</strong></font>;  <font color=gray><strong>'+low_temp+'</strong></font><br>日出时间：'+sunrise\
+	+'  日落时间：'+sunset+'<br><br><font color=green><strong>温馨提醒：</strong></font><strong>'+notice+'</strong><br><br>')
+	return weather_content
 
-		#art_url=tdlink.find('href')
-		#print(art_url)
+if __name__ == '__main__':
 	content_list1=get_content_xgb()
 	content_list2=get_content_tw()
 	content_list3=get_content_jyzx()
-	content_list=content_list1+content_list2+content_list3
+	content_list4=get_content_ygb()
+	content_list5=get_weather()
+	content_list=content_list1+content_list2+content_list3+content_list4+content_list5
 	print(content_list)
+	f=open("D:/Documents/Python/log/logfile.txt",'a',encoding='utf-8') #在定时任务中，filepath一定要用绝对路径
+	f.write('\n'.join(content_list)+'\r\n') #list输出到txt的一种方法，也可以用for...in...的语句
+	f.close()
 	mail_content=''
 	if len(content_list)==0:
 		sent_email('今天没有邮件')
 	else:
 		#sent_email(content_list)
 		for content in content_list:
-			mail_content=mail_content+content+'\r\n\n'
+			mail_content=mail_content+content+'<br><br>'
 		sent_email(mail_body=mail_content)
 	content_list.clear()
